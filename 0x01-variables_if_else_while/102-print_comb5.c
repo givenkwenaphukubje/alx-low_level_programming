04aa8f454f7c0b6190ce180ca95be5346ada54c2
givenkwenaphukubje <givenkwenaphukubje@gmail.com>
2023-03-05 07:28:12 -0800

    main.h

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns a combinatoin of number
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (j = 0; j < 100; j++)
+	{
+		if (i < j)
+		{
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar (' ');
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			if (i != 98 || j != 98)
+			{
+				putchar(',");
+				putchar(' ');
+			)
+		}
+	)
+}
+
+putchar('\n")
+
+return (0):
+}
